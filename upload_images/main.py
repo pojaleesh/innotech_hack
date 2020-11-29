443e824bab1d44bfbe7f54cfafa2c7c36bb27fa8
adil <adhan2a@gmail.com>
2020-11-29 04:22:11 +0300

    new_html

--- upload_images/main.py
+++ upload_images/main.py
@@ -17,8 +17,8 @@ def allowed_image(filename):
 
     ext = filename.rsplit(".", 1)[1]
 
-    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
-        return True
+if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+    return True
     else:
         return False
 
@@ -33,29 +33,31 @@ def allowed_image_filesize(filesize):
 @app.route("/upload-image", methods=["GET", "POST"])
 def upload_image():
     if request.method == "POST":
-
+        f = open('links', 'w')
+        
         if request.files:
-
+            f.write(request.form['name'])
             image = request.files["image"]
-
             if image.filename == "":
                 print("No filename")
                 return redirect(request.url)
-
+        
             if allowed_image(image.filename):
                 filename = secure_filename(image.filename)
+                
                 image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
-
+                
                 print("Image saved")
-
+                
                 return redirect(request.url)
-
+            
             else:
                 print("That file extension is not allowed")
                 return redirect(request.url)
 
-    return render_template("upload_image.html")
+return render_template("upload_image.html")
 
 
 if __name__ == "__main__":
-    app.run(port=4567)
+    app.run(host='0.0.0.0', port=4567)
+
