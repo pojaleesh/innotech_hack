2da5c7b1a118061dcb0292b04df0b67029d27de3
Chop <ch00p.228@gmail.com>
2020-11-29 03:59:06 +0300

    Was changed lang

--- vk_alg/ParsFromVK.py
+++ vk_alg/ParsFromVK.py
@@ -6,21 +6,21 @@ token = 'dda4eeaddda4eeaddda4eeadaaddd61e23ddda4dda4eead82a032200963ecfbb7fcf175
 def _information_about_user(user_link):
     global token
     fields = 'city,about,bdate,education,interests,career,universities,schools,occupation'
-    response = requests.get(f'https://api.vk.com/method/users.get?access_token={token}&user_ids={user_link}&v=5.120&fields={fields}')
+    response = requests.get(f'https://api.vk.com/method/users.get?lang=0&access_token={token}&user_ids={user_link}&v=5.120&fields={fields}')
     return response.json()['response'][0]
 
 
 # Парсинг друзей
 def _ids_of_users_friends(user_id):
     global token
-    response = requests.get(f'https://api.vk.com/method/friends.get?access_token={token}&user_id={user_id}&v=5.120&return_system=0&order=name')
+    response = requests.get(f'https://api.vk.com/method/friends.get?lang=0&access_token={token}&user_id={user_id}&v=5.120&return_system=0&order=name')
     return response.json()['response']['items']
 
 
 # Парсинг групп
 def _information_about_users_groups(user_id):
     global token
-    response = requests.get(f'https://api.vk.com/method/users.getSubscriptions?access_token={token}&user_id={user_id}&v=5.120&extended=1')
+    response = requests.get(f'https://api.vk.com/method/users.getSubscriptions?lang=0&access_token={token}&user_id={user_id}&v=5.120&extended=1')
     return response.json()['response']['items']
 
 
@@ -46,6 +46,11 @@ def pars_from_vk(user_id):
     information_about_users_groups = temp
     return (information_about_user, information_about_users_groups)
 
+def get_info(user_id):
+    global token
+    fields = 'photo_max'
+    response = requests.get(f'https://api.vk.com/method/users.get?lang=0&access_token={token}&user_ids={user_id}&v=5.120&fields={fields}')
+    return response.json()['response'][0]
 
 def main():
     user = 'nomapunkkk'
